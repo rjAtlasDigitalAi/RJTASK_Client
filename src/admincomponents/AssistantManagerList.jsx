5cb22164d851158baba277703afd90cb8566dd8e
vishnuaxobis <vishnu@axobis.com>
2026-02-03 10:43:59 +0530

    just verify

--- src/admincomponents/AssistantManagerList.jsx
+++ src/admincomponents/AssistantManagerList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useEffect, useState} from "react";
 // import api from "../api";
 // import { AuthContext } from "../AuthContext";
 import "./AssistantManagerList.css";
